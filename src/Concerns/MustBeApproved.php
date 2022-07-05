978cd8cfeb24689c655962bbe57215a2e8b800b1
Chris Mellor <cmellor@gmail.com>
2022-07-05 23:38:33 +0100

    chore: Refactor trait

--- src/Concerns/MustBeApproved.php
+++ src/Concerns/MustBeApproved.php
@@ -2,9 +2,9 @@
 
 namespace Cjmellor\Approval\Concerns;
 
+use Cjmellor\Approval\Enums\ApprovalStatus;
 use Cjmellor\Approval\Models\Approval;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
-use Illuminate\Support\Collection;
 
 trait MustBeApproved
 {
@@ -12,38 +12,73 @@ trait MustBeApproved
 
     public static function bootMustBeApproved(): void
     {
-        static::saved(callback: function ($model) {
-            if (! $model->wasChanged()) {
-                return;
-            }
+        static::creating(callback: fn ($model) => static::insertApprovalRequest($model));
+        static::updating(callback: fn ($model) => static::insertApprovalRequest($model));
+    }
 
-            if ($model->isApprovalBypassed()) {
-                return;
+    /**
+     * Create an Approval request before committing to the database.
+     */
+    protected static function insertApprovalRequest($model)
+    {
+        if (! $model->isApprovalBypassed()) {
+            /**
+             * Create the new Approval model
+             */
+            if (self::approvalModelExists($model)) {
+                return false;
             }
 
             $model->approvals()->create([
                 'new_data' => $model->getDirty(),
                 'original_data' => $model->getOriginalMatchingChanges(),
             ]);
-        });
+
+            return false;
+        }
     }
 
-    public function isApprovalBypassed(): bool
+    /**
+     * Check if the Approval model been created already exists with a 'pending' state
+     */
+    protected static function approvalModelExists($model): bool
     {
-        return $this->bypassApproval;
+        return Approval::where([
+            ['state', '=', ApprovalStatus::Pending],
+            ['new_data', '=', json_encode($model->getDirty())],
+            ['original_data', '=', json_encode($model->getOriginalMatchingChanges())],
+        ])->exists();
     }
 
+    /**
+     * The polymorphic relationship for the Approval model.
+     */
     public function approvals(): MorphMany
     {
         return $this->morphMany(related: Approval::class, name: 'approvalable');
     }
 
-    protected function getOriginalMatchingChanges(): Collection
+    /**
+     * Gets the original model data and only gets the keys that match the dirty attributes.
+     */
+    protected function getOriginalMatchingChanges(): array
     {
         return collect($this->getOriginal())
-            ->only(collect($this->getChanges())->keys());
+            ->only(collect($this->getDirty())->keys())
+            ->toArray();
     }
 
+    /**
+     * Check is the approval can be bypassed.
+     */
+    public function isApprovalBypassed(): bool
+    {
+        return $this->bypassApproval;
+    }
+
+    /**
+     * Approval is ignored and persisted to the database.
+     */
     public function withoutApproval(): static
     {
         $this->bypassApproval = true;
