fea425e008e75fc3603d30350664f161f61ea046
Scott Mankowitz <scott.mankowitz@stationmd.com>
2022-08-31 23:52:57 -0400

    Automatic Commit - HOTFIX

--- src/Concerns/MayBeApproved.php
+++ src/Concerns/MayBeApproved.php
@@ -4,24 +4,22 @@ namespace Cjmellor\Approval\Concerns;
 
 trait MayBeApproved
 {
-
   use MustBeApproved;
   protected static bool $requiresApproval = false;
 
+  /**
+   * Check if the approval can be bypassed, based on static $requiresApproval flag;
+   */
   public function isApprovalBypassed(): bool
   {
     return !self::$requiresApproval;
   }
 
+  /**
+   * Sets the $requiresApproval flag (defaults to true)
+   */
   public static function requireApproval($requires = true)
   {
     self::$requiresApproval = $requires;
-  }
-
-  public function withApproval(): static
-  {
-    $this->bypassApproval = false;
-    self::$requiresApproval = true;
-    return $this;
   }
 }
