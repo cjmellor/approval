4d44878faa6c301ef7c1a6cf005877fd8987aa2c
Nuno Donato <nunodonato@fastmail.com>
2023-11-14 11:06:15 +0000

    added the model and the user to all event constructors

--- tests/Feature/Scopes/ApprovalStateScopeTest.php
+++ tests/Feature/Scopes/ApprovalStateScopeTest.php
@@ -3,7 +3,6 @@
 use Cjmellor\Approval\Enums\ApprovalStatus;
 use Cjmellor\Approval\Events\ModelApproved;
 use Cjmellor\Approval\Events\ModelRejected;
-use Cjmellor\Approval\Events\ModelRolledBackEvent;
 use Cjmellor\Approval\Events\ModelSetPending;
 use Cjmellor\Approval\Models\Approval;
 use Cjmellor\Approval\Tests\Models\FakeModel;
@@ -97,7 +96,7 @@ it(description: 'only changes the status of the requested model', closure: funct
 });
 
 test(description: 'An event is fired when a Model\'s state is changed', closure: function (string $state): void {
-    $fakeModel = FakeModel::create($this->fakeModelData);
+    FakeModel::create($this->fakeModelData);
 
     Event::fake();
 
