c05d5cc9d5420202a34b333fa1c7c78c84b3f2c9
Chris Mellor <cmellor@gmail.com>
2025-04-05 18:30:10 +0700

    refactor: update ApprovalServiceProvider to use Laravel's ServiceProvider and enhance package registration

--- src/ApprovalServiceProvider.php
+++ src/ApprovalServiceProvider.php
@@ -3,24 +3,47 @@
 namespace Cjmellor\Approval;
 
 use Cjmellor\Approval\Console\Commands\ProcessExpiredApprovalsCommand;
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
-class ApprovalServiceProvider extends PackageServiceProvider
+class ApprovalServiceProvider extends ServiceProvider
 {
-    public function configurePackage(Package $package): void
+    /**
+     * Bootstrap any package services.
+     */
+    public function boot(): void
     {
-        $package
-            ->name(name: 'approval')
-            ->hasConfigFile()
-            ->hasCommand(commandClassName: ProcessExpiredApprovalsCommand::class)
-            ->hasMigrations([
-                '2022_02_12_195950_create_approvals_table',
-                '2023_10_09_204810_add_rolled_back_at_column_to_approvals_table',
-                '2023_11_17_002135_add_audited_by_column_to_approvals_table',
-                '2024_03_16_173148_add_foreign_id_column_to_approvals_table',
-                '2025_03_04_000001_add_creator_to_approvals_table',
-                '2025_03_14_062513_add_expiration_columns_to_approvals_table',
+        // Register config file
+        $this->publishes(
+            paths: [__DIR__.'/../config/approval.php' => config_path(path: 'approval.php')],
+            groups: 'approval-config'
+        );
+
+        // Register migrations
+        $this->publishes(
+            paths: [__DIR__.'/../database/migrations/' => database_path(path: 'migrations')],
+            groups: 'approval-migrations'
+        );
+
+        // Load migrations
+        $this->loadMigrationsFrom(paths: __DIR__.'/../database/migrations');
+
+        // Register commands
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                ProcessExpiredApprovalsCommand::class,
             ]);
+        }
+    }
+
+    /**
+     * Register any package services.
+     */
+    public function register(): void
+    {
+        // Merge config
+        $this->mergeConfigFrom(
+            path: __DIR__.'/../config/approval.php',
+            key: 'approval'
+        );
     }
 }
