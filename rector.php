2560f87938589ec3a7dbb9f99f51e7fe16de2519
Chris Mellor <cmellor@gmail.com>
2025-04-27 17:47:57 +0700

    build: add rector configuration and dependencies
    Introduce rector.php for code refactoring and upgrade rules specific to Laravel. Add rector and rector-laravel dependencies to composer.json to enable automated code improvements and maintainability enhancements.

---
+++ rector.php
@@ -0,0 +1,28 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use RectorLaravel\Set\LaravelSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/config',
+        __DIR__.'/database',
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true,
+    )
+    ->withImportNames(removeUnusedImports: true)
+    ->withPhpSets(php83: true)
+    // Used with the Laravel rector plugin.
+    ->withSets([
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_COLLECTION,
+        LaravelSetList::LARAVEL_IF_HELPERS,
+    ]);
